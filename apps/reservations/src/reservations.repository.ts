b7f7212556767326c0e7f545f710eefb89154675
Daniel Román <88067941+DanielRoman11@users.noreply.github.com>
2024-10-21 05:34:12 +0000

    feat: Reservations as Microservice

---
+++ apps/reservations/src/reservations.repository.ts
@@ -0,0 +1,7 @@
+import { AbstractRepository } from "@app/common/database/abstract.repository";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class ReservationsRepository extends AbstractRepository{
+
+}
