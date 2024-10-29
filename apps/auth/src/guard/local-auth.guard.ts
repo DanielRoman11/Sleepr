a27fde0d6f7578b47092acc2d748b24f8cf94796
Daker <danielroman.05111@gmail.com>
2024-10-28 22:33:41 -0500

    feat: LocalAuthGuard: throwing more informative validation err

--- apps/auth/src/guard/local-auth.guard.ts
+++ apps/auth/src/guard/local-auth.guard.ts
@@ -1,5 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+  handleRequest<TUser = any>(
+    err: any,
+    user: any,
+    info: any,
+    context: ExecutionContext,
+    status?: any,
+  ): TUser {
+    if (err || !user) {
+      throw err || new UnauthorizedException('Invalid credentials provided');
+    }
+    return user;
+  }
+}
