454b2d7290d6ecaa407318182b22b5cafb54cf35
Daker <danielroman.05111@gmail.com>
2024-11-04 02:43:43 -0500

    fix: PORT changes to HTTP_PORT

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   app.use(cookieParser());
   app.useLogger(app.get(Logger));
   const configServie = app.get(ConfigService);
-  await app.listen(configServie.get('PORT'));
+  await app.listen(configServie.get('HTTP_PORT'));
 }
 bootstrap();
